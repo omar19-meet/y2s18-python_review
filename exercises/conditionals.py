9cec91606753e73ab481f728a1ae1a3384ce2658
meet <omar19@meet.mit.edu>
2018-08-05 09:30:50 -0400

    lol

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,11 @@
 # Write your solution for 1.2 here!
+# a=0
+# for i in range(101):
+#     if i%2==0: 
+#         a=a+i
+# print(a)        
+for i in range(1000,0,-1):
+    if i%6==2:
+        if i**3%5==3:
+            print(i)
+            break
